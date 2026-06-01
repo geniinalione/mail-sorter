6fde63d54cc2b704f8d2f0693fdc4bbfeabc4ac8
MacBook <macbook@MacBook-ka-MacBook-Air.local>
2026-06-02 02:25:51 +0300

    Change run.sh, now work on Windows

--- run.sh
+++ run.sh
@@ -2,7 +2,16 @@
 
 if [ -d "data/inbox" ]; then
     echo "start mail-sorter"
-    python3 -m mailsorter.pipeline
+
+    if command -v python3 >/dev/null 2>&1; then
+        PY=python3
+    elif command -v py >/dev/null 2>&1; then
+        PY=py
+    else
+        PY=python
+    fi
+
+    "$PY" -m mailsorter.pipeline
     echo "end mail-sorter"
 else
     echo "There is no data/inbox directory"
