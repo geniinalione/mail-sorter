df002f625ca5b34f20a65901353426735ecd81e6
radmirmudarisov77-sketch <radmirmudarisov77@gmail.com>
2026-06-01 12:18:55 +0300

    Implement EmailReader class for parsing emails

---
+++ mailsorter/reader.py
@@ -0,0 +1,49 @@
+class EmailReader:
+    def _parse_headers(self, header_block: str):
+        data = {"from": "sender", "от кого": "sender", "ot kogo": "sender", "subject": "subject", "тема": "subject",
+                "tema": "subject"}
+        result = {}
+        for line in header_block.splitlines():
+            tek = line.split(":", 1)
+            if len(tek) == 1:
+                continue
+            key = tek[0].strip().lower()
+            znach = tek[1].strip()
+            if key in data:
+                result[data[key]] = znach
+        return result
+
+    def _extract_domain(self, sender: str):
+        if "@" not in sender:
+            return ""
+        else:
+            tek = sender.split("@")[-1]
+            tek = tek.strip(" >")
+            return tek
+
+    def _split_headers_body(self, text: str):
+        mail = text.split("\n\n", 1)
+        if len(mail) == 2:
+            return (mail[0], mail[1])
+        else:
+            return (mail[0], "")
+
+    def _extract_text (self, path: Path) -> str:
+        try:
+            data = path.read_bytes()
+            best = from_bytes(data).best()
+            if best is None:
+                raise UnreadableEmailError
+            else:
+                return str(best)
+        except Exception:
+            raise UnreadableEmailError
+
+    def read(self, path: Path) -> EmailMessage:
+        text = self._extract_text(path)
+        header, body = self._split_headers_body(text)
+        hd = self._parse_headers(header)
+        sender = hd.get('sender', '')
+        domain = self._extract_domain(sender)
+        subject = hd.get('subject', '')
+        return EmailMessage(sender, domain, subject, body, path, text)
