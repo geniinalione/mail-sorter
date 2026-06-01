58c95754e85d1a89e9fc7a90a1956210084edc92
Mudarisov Radmir <railmudarisov@edu.hse.ru>
2026-06-01 19:54:48 +0300

    classifier finance, hr, support, external are complete

--- mailsorter/classifier.py
+++ mailsorter/classifier.py
@@ -1,7 +1,11 @@
 from .email_message import EmailMessage
 class Classifier:
-    critical_words = ["массов","сбой","критич"]
+    critical_words = ["массов","сбой","критич", "падает", "работа полностью остановлена"]
     spam_words = ["выигр", "скидк", "акци", "заблокир", "перейдите", "истекает", "бесплатно", "розыгрыш","внимание"]
+    finance_words = ["счет", "счёт", "оплат", "договор"]
+    hr_words = ["график","отпуск","больничн","bolnichn"]
+    internal_words = ["company.ru", "corp.local", ".internal"]
+    support_words = ["ошибк", "помог", "нужны права", "нужен доступ", "перестал", "не мог", "не открыв"]
     def classify(self, msg: EmailMessage) -> str:
         if self._is_spam(msg):
             return "spam"
@@ -33,10 +37,27 @@ class Classifier:
     def _is_automated(self, msg: EmailMessage):
         return msg.domain.lower().endswith(".internal")
     def _is_finance(self,msg:EmailMessage):
+        text = (msg.subject + " " + msg.body).lower()
+        for word in self.finance_words:
+            if word in text:
+                return True
         return False
     def _is_hr(self,msg:EmailMessage):
-        return False
-    def _is_external(self,msg:EmailMessage):
+        text = (msg.subject + " " + msg.body).lower()
+        for word in self.hr_words:
+            if word in text:
+                return True
         return False
     def _is_support(self,msg:EmailMessage):
+        text = (msg.subject + " " + msg.body).lower()
+        for word in self.support_words:
+            if word in text:
+                return True
         return False
+    def _is_external(self,msg:EmailMessage):
+        if msg.domain == '':
+            return False
+        for word in self.internal_words:
+            if word in msg.domain:
+                return False
+        return True
